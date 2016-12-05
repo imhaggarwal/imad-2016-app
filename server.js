a587ee328978d8f56dc322770c46489960712bdc
imhaggarwal <aggarwalh.com@gmail.com>
2016-12-06 01:03:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto=require('crypto');
 
 var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input, 'random', 100, 100, 'sha512');
+    return hashed.toString('hex');
+}
+
+app.get('/hash/:input', function(req,res){
+   var hashString=hash(input);
+   res.end(hashString);
 });
 
 app.get('/ui/style.css', function (req, res) {
