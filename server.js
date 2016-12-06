daf950373c96503c9335b59e5ab56f668d820386
imhaggarwal <aggarwalh.com@gmail.com>
2016-12-06 09:46:03 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.end('finally');
 });
 
 app.get('/ui/style.css', function (req, res) {
