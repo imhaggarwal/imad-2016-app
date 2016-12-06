af646c9a82537065ddade287548d7edc81a7c49b
imhaggarwal <aggarwalh.com@gmail.com>
2016-12-06 09:44:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 //var crypto=require('crypto');
@@ -7,12 +7,10 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  //res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-  res.end('Finally');
-  console.log('Data Fetched');
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-/*function hash(input){
+function hash(input){
     var hashed=crypto.pbkdf2Sync(input, 'random', 100, 100, 'sha512');
     return hashed.toString('hex');
 }
@@ -20,15 +18,41 @@ app.get('/', function (req, res) {
 app.get('/hash/:input', function(req,res){
    var hashString=hash(input);
    res.end(hashString);
-});app.get('/ui/style.css', function (req, res) {
+});
+
+app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
 */
+var express = require('express');
+var morgan = require('morgan');
+var path = require('path');
+
+var app = express();
+app.use(morgan('combined'));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+app.listen(8080, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
