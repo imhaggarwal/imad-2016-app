1029c1d103d0246af0db5412d7ffac408871122b
imhaggarwal <aggarwalh.com@gmail.com>
2016-12-06 09:34:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,9 +18,7 @@ app.get('/', function (req, res) {
 app.get('/hash/:input', function(req,res){
    var hashString=hash(input);
    res.end(hashString);
-});*/
-
-/*app.get('/ui/style.css', function (req, res) {
+});app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
